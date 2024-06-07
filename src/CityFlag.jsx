12989213ad54f2d5e41ec0e777015c29f5cd41c6
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-07 18:05:49 +0530

    update alt

--- src/CityFlag.jsx
+++ src/CityFlag.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import CountryCard from "./CountryCard";
+
 function CityFlag() {
   const [cityData, setCityData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -7,7 +9,7 @@ function CityFlag() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://restcountries.com/v3.1/all',{ timeout: 10000 }); // increase timeout to 10 seconds
+        const response = await fetch('https://restcountries.com/v3.1/all'); // increase timeout to 10 seconds
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
@@ -25,17 +27,24 @@ function CityFlag() {
   }, []);
 
   return (
-    <div className="maindiv">
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        cityData.map((data, index) => (
-          <div key={index} className="innerdiv">
-            <img src={data.flags.png} alt={data.name?.common} style={{ height: "100px", width: "100px" }} />
-            <h5>{data.name?.common}</h5>
-          </div>
-        ))
-      )}
+    <div 
+     style={{
+      display: "flex",
+      flexWrap : "wrap",
+      alignItems: "center",
+      justifyContent: "center",
+      height : "100vh"
+
+    }}
+    >
+      {cityData.map((data, index) => (
+        <CountryCard
+          key={index}
+          countryName={data.name.common}
+          imageUrl={data.flags.png}
+          flagAltText={data.flags.alt || data.name.common} // fallback to country name if alt is not available
+        />
+      ))}
     </div>
   );
 }
