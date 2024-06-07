f4d89961b841893abde97e4d1cde2917c3a0780f
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-07 19:09:56 +0530

    creating loading..

--- src/CityFlag.jsx
+++ src/CityFlag.jsx
@@ -5,27 +5,28 @@ import CountryCard from "./CountryCard";
 function CityFlag() {
   const [cityData, setCityData] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+  const getCountries = async () => {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log('Received data:', data);
+      setCityData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Failed to fetch:', error);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log('Received data:', data);
-        setCityData(data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Failed to fetch:', error);
-        setLoading(false);
-      }
-    };
+    
 
-    fetchData();
+    getCountries();
   }, []);
+  console.log(cityData)
 
   if (loading) {
     return <h1>Loading...</h1>;
@@ -35,9 +36,9 @@ function CityFlag() {
 
   return (
     <div className="maindiv">
-      {cityData.map((data, index) => (
+      {cityData.map((data) => (
         <CountryCard
-          key={index}
+          key={data.flags.alt}
           countryName={data.name.common}
           imageUrl={data.flags.png}
           flagAltText={data.flags.alt}
