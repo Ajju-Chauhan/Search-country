dd41d7bdf2f9bc5a103eb6ca8765d8477fb8b712
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-07 18:16:48 +0530

    update alt

--- src/CityFlag.jsx
+++ src/CityFlag.jsx
@@ -7,24 +7,13 @@ function CityFlag() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://restcountries.com/v3.1/all'); // increase timeout to 10 seconds
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log('Received data:', data); // log the data for debugging
-        setCityData(data);
-        setLoading(false); // Set loading to false when data is fetched
-      } catch (error) {
-        console.error('Failed to fetch:', error);
-        setLoading(false); // Set loading to false on error
-      }
-    };
-
-    fetchData();
+    let URL = 'https://restcountries.com/v3.1/all'
+    fetch(URL)
+    .then((res)=> res.json())
+    .then((data)=> setCityData(data))
+    .catch((err)=> console.error("Error", err))
   }, []);
+  console.log(cityData);
 
   return (
     <div 
