2c28e397fbba4060622e26197bad53e2a597420b
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-07 18:37:48 +0530

    update alt

--- src/CountryCard.jsx
+++ src/CountryCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
-const CountryCard = ({countryName, imageUrl ,flagAltText}) => {
+const CountryCard = ({ countryName, imageUrl, flagAltText }) => {
   return (
-    <div style={{
+    <div
+      style={{
         display: "flex",
         justifyContent: "center",
-        flexDirection:"column",
-        alignItems : "center",
-        padding : "10px",
-        margin : "10px",
-        border : "1px solid black",
-         borderRadius: "8px", width: "200px", height: "200px", margin: "10px", textAlign: "center" }}>
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "10px",
+        margin: "10px",
+        border: "1px solid black",
+        borderRadius: "8px",
+        width: "200px",
+        height: "200px",
+        textAlign: "center",
+      }}
+    >
       <img src={imageUrl} alt={flagAltText} style={{ height: "100px", width: "100px" }} />
       <h2>{countryName}</h2>
     </div>
