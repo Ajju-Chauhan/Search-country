c14be28249c17ca21641acbc2f6b49385932d260
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-07 15:42:39 +0530

    update alt

--- src/CityFlag.jsx
+++ src/CityFlag.jsx
@@ -1,38 +1,22 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function CityFlag() {
   const [cityData, setCityData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const controller = new AbortController();
-        const signal = controller.signal;
-
-        const timeoutId = setTimeout(() => {
-          controller.abort();
-        }, 10000); // 10 seconds timeout
-
-        const response = await fetch('https://restcountries.com/v3.1/all', { signal });
+        const response = await fetch('https://restcountries.com/v3.1/all'); // increase timeout to 10 seconds
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-
         const data = await response.json();
         console.log('Received data:', data); // log the data for debugging
         setCityData(data);
         setLoading(false); // Set loading to false when data is fetched
-
-        clearTimeout(timeoutId);
       } catch (error) {
-        if (error.name === 'AbortError') {
-          console.error('Fetch aborted');
-        } else {
-          console.error('Failed to fetch:', error);
-        }
-        setError(error.message);
+        console.error('Failed to fetch:', error);
         setLoading(false); // Set loading to false on error
       }
     };
@@ -40,22 +24,18 @@ function CityFlag() {
     fetchData();
   }, []);
 
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (error) {
-    return <h1>Error: {error}</h1>;
-  }
-
   return (
     <div className="maindiv">
-      {cityData.map((data, index) => (
-        <div key={index} className="innerdiv">
-          <img src={data.flags?.png} alt={data.name?.common} style={{ height: "100px", width: "100px" }} />
-          <h5>{data.name?.common}</h5>
-        </div>
-      ))}
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        cityData.map((data, index) => (
+          <div key={index} className="innerdiv">
+            <img src={data.flags.png} alt={data.name?.common} style={{ height: "100px", width: "100px" }} />
+            <h5>{data.name?.common}</h5>
+          </div>
+        ))
+      )}
     </div>
   );
 }
