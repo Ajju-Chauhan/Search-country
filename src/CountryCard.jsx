cbe2388b2b2466f575867debb3306d4894052b52
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-12 17:37:37 +0530

    check card test cases..

--- src/CountryCard.jsx
+++ src/CountryCard.jsx
@@ -1,20 +1,13 @@
-import React from "react";
+// CountryCard.js
+import React from 'react';
 
-const CountryCard = ({countryName, imageUrl ,flagAltText}) => {
+function CountryCard({ countryName, imageUrl, flagAltText, className }) {
   return (
-    <div style={{
-        display: "flex",
-        justifyContent: "center",
-        flexDirection:"column",
-        alignItems : "center",
-        padding : "10px",
-        margin : "10px",
-        border : "1px solid black",
-         borderRadius: "8px", width: "200px", height: "200px", margin: "10px", textAlign: "center" }}>
-      <img src={imageUrl} alt={flagAltText} style={{ height: "100px", width: "100px" }} />
-      <h2>{countryName}</h2>
+    <div className="countryCard">
+      <img src={imageUrl} alt={flagAltText} />
+      <p>{countryName}</p>
     </div>
   );
-};
+}
 
 export default CountryCard;
